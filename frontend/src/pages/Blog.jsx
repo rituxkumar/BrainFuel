3906b76f71d36d27f06246b143a14ee7571571c5
Ritu kumar <ritu748841@gmail.com>
2025-08-14 15:34:42 +0530

    process

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { assets, blog_data } from "../assets/assets";
+import { assets, blog_data, comments_data } from "../assets/assets";
 import Moment from 'moment';
 import Navbar from "../components/Navbar";
 
 const Blog = () => {
   const { id } = useParams();
   const [data, setData] = useState(null);
+  const [comments,setComments] = useState([])
 
   const fetchBlogData = async () => {
     const data = blog_data.find(item => item._id === id);
     setData(data);
   };
 
+  const fetchComments = async () =>{
+    setComments(comments_data)
+  }
+
   useEffect(() => {
-    fetchBlogData();
+    fetchBlogData()
+    fetchComments()
   }, []);
 
   return data ? (
@@ -32,6 +38,27 @@ const Blog = () => {
       <h1 className="text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800">{data.title}</h1>
       <h2 className="my-5 max-w-lg truncate mx-auto">{data.subTitle}</h2>
       <p className="inline-block py-1 px-4 rounded-full mb-6  border text-sm border-primary/5 font-medium text-primary">Ritu kumar</p>
+    </div>
+
+    <div className="mx-5 max-w-5xl md:mx-auto my-10 mt-6">
+      <img src={data.image} alt="" className="rounded-3xl mb-5"/>
+      <div className="rich-text max-w-3xl mx-auto" dangerouslySetInnerHTML={{__html: data.description}}></div>
+
+      <div className="mt-14 mb-10 max-w-3xl mx-auto">
+        <p className="font-semibold mb-4">Comments ({ comments.length })</p>
+         <div className="flex flex-col gap-4">
+        {comments.map((item,index)=>(
+          <div key={index} className="relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600">
+              <div className="flex items-center gap-2 mb-2">
+                <img src={assets.user_icon} alt="" className="w-6"/>
+                <p className="font-medium">{item.name}</p>
+              </div>
+              <p className="text-sm max-w-md ml-8">{item.content}</p>
+              <div className="absolute right-4 bottom-3 flex items-center gap-2 text-xs">{Moment(item.createdAt).fromNow()}</div>
+          </div>
+        ))}
+         </div>
+      </div>
     </div>
 
    </div>
