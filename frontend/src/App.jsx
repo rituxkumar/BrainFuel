a7a1ea29c33c13b95f9f48461a507a4fc7986823
Ritu kumar <ritu748841@gmail.com>
2025-08-16 10:36:03 +0530

    process

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import AddBlog from "./pages/admin/AddBlog";
 import Comments from "./pages/admin/Comments";
 import Dashboard from "./pages/admin/Dashboard";
 import ListBlog from "./pages/admin/ListBlog";
+import Login from "./components/admin/Login";
 
 
 
@@ -16,7 +17,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/admin" element={<Layout />}>
+        <Route path="/admin" element={false ?<Layout />:<Login />}>
           <Route index element={<Dashboard/>} />
           <Route path="addBlog" element={<AddBlog />} />
           <Route path="listBlog" element={<ListBlog />} />
