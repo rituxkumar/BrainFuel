0fa1d8d1284ab3b16a219aa435ba82d81531819f
Ritu kumar <ritu748841@gmail.com>
2025-08-17 19:23:54 +0530

    process

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,3 +1,5 @@
+import imagekit from "../configs/imageKit";
+import Blog from "../models/Blog.js";
 
 
 
@@ -11,8 +13,38 @@ export const addBlog = async (req,res) =>{
     if(!title || !description || !category || !imageFile){
       return res.json({success:false,message:"Missing required fields"})
     }
+
+    const fileBuffer = fs.readFileSync(imageFile.path)
+
+    // upload image to imagekit
+
+    const response = await imagekit.upload({
+      file:fileBuffer,
+      fileName:imageFile.originalname,
+      folder:"/blogs"
+    })
+
+    // optimization through imagekit url transformation
+   
+    const optimizedImageUrl = imagekit.url({
+      path:response.filePath,
+      transformation:[
+        {quality:'auto'}, // auto compression
+        {format:'webp'}, // convert to modern format
+        {width:'1280'} // width resizing
+      ]
+    });
+
+    const image = optimizedImageUrl;
+
+   await Blog.create({title,subTitle,description,category,image,isPublished})
+
+   res.json({success:true,message:"Blog added successfully 🚀"})
+
      
   } catch (error) {
+
+    res.json({success:false,message:error.message})
     
   }
 }
