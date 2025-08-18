be25806752d243cca43ba24188114389de02dea5
Ritu kumar <ritu748841@gmail.com>
2025-08-18 17:50:38 +0530

    process

---
+++ frontend/context/AppContext.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const value = {};
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
