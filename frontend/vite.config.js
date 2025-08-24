521bd042d108dfbcca88c336a9a19c55a2da03bb
Ritu kumar <ritu748841@gmail.com>
2025-08-24 23:28:45 +0530

    process

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()],
+  plugins: [vue()],
 })
